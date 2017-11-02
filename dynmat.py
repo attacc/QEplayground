fe26218d0bd0eb81f681f391e206bc3941daa2f0
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2017-11-02 10:07:23 +0100

    Added more units

--- dynmat.py
+++ dynmat.py
@@ -14,13 +14,12 @@ class dynmat:
     """
     def __init__(self, qe_input, filename=None):
         self.qe_input=qe_input
-        self.freq_pattern=r'\s*freq \(\s*(\d*)\) =\s*('+r_or_d+')\s\[THz\]'
         self.nat        = int(self.qe_input.system['nat'])
         self.nmodes     = 3*int(self.nat)
-        self.nqpoints   = 1  #Only at gamma for the momemt
+        self.qvec       = []
+        #
         if(filename):
             self.read_dyn_output(filename)
-        
 
     def __del__(self):
         print("Destroy class Dynmat")
@@ -30,8 +29,9 @@ class dynmat:
         for iq in range(0,self.nqpoints):
             string+="q = \n"
             for im in range(0,self.nmodes):
-                string+="  mode (   %d) \n" % (im+1)
-#                for ia in range(0,self.nat):
+                string+="\tmode (   %d) \n" % (im+1)
+                for ia in range(0,self.nat):
+                    string+="\t\t "+str(self.pol_vec[iq][im][ia])+"\n"
         return string
 
     def read_dyn_output(self, filename):
@@ -39,16 +39,21 @@ class dynmat:
         self.file_lines=ifile.readlines()
         lines = iter(self.file_lines)
         ifile.close()
-        regex=re.compile(self.freq_pattern)
-        self.pol_vec=np.zeros((self.nqpoints,self.nmodes,self.nat,3),dtype=complex)
-
-        for line in lines:
-            match=regex.match(line)
-            if match is not None:
-                imode=int(match.group(1))-1  # Mode number
-                for ia in range(self.nat):
-                    tmp_vec     =re.findall(r_or_d, next(lines))
-                    self.pol_vec[0][imode][ia].real=tmp_vec[0::2]
-                    self.pol_vec[0][imode][ia].imag=tmp_vec[1::2]
+        #
+        # Find the number of q-points
+        #
+        qpattern="q ="
+        self.nqpoints=sum(qpattern in line for line in lines)
+        #
+        g
+#        for line in lines:
+#            if qpattern in line:
+#                qline, vec = line.split()
+#                qvec.append(vec)
+#                next(lines) # the line with asterix
+#                for im in range(0,nmodes):
+#                    for ia in range(self.nat):
+#                        tmp_vec     =re.findall(r_or_d, next(lines))
+#                        vec=tmp_vec[0::2]+1j*tmp_vec[1::2]
 
 
