aec99d310d9a694c82e4d2b4132684e7ccfdc221
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2017-11-29 10:59:21 +0100

    Fixed frozen-phonons and examples

---
+++ QEplayground/examples/Diamond1x1x1/test_fp.py
@@ -0,0 +1,27 @@
+from QEplayground.pwscf  import *
+from QEplayground.matdyn import *
+from QEplayground.frozen_phonons import *
+
+r_order=2    # Richardson extrapolation order 
+delta=0.02  # Displacement in a.u.
+
+scf_filename    ="diamond.scf.in"
+dynmat_filename ="dynmat.out"
+
+qe_input =Pwscf(scf_filename)
+qe_dyn=Matdyn(qe_input,dynmat_filename)
+
+pw="/home/attacc/SOFTWARE/qe-6.1/bin/pw.x"
+
+#Serial job
+#qe_input.set_run_options(pw=pw)
+
+#Parallel job
+qe_input.set_run_options(pw=pw, nprocs=2, npool=2)
+
+
+# Pseudo-potential directory
+qe_input.control['pseudo_dir']="'/home/attacc/SOFTWARE/PSEUDO_PWSCF'"
+
+frozen_phonons(qe_input, qe_dyn, delta, r_order=r_order)
+
--- QEplayground/frozen_phonons.py
+++ QEplayground/frozen_phonons.py
@@ -12,107 +12,97 @@ from QEplayground.pwout  import *
 from QEplayground.units  import autime2s,amu2au,thz2cm1
 import math
 
+def frozen_phonons(qe_input, qe_dyn, delta, r_order=2, modes=None):
 
-r_order=3    # Richardson extrapolation order 
-delta=0.02  # Displacement in a.u.
+    masses=qe_input.get_masses()
+    ref_mass=max(masses)
+    M       =1.0/(np.sum(np.reciprocal(masses)))
+    M       =M*float(qe_input.system['nat'])
 
-scf_filename    ="hBN.scf.in"
-dynmat_filename ="dynmat.out"
+    string="\n\n* * * Frozen phonon calculations * * *\n\n"
+    string+="Reference  mass : %12.8f \n" % ref_mass
+    string+="Reduced    mass : %12.8f [ref_mass units]  %12.8f  [amu]\n" % (M/ref_mass,M )
 
+    print(string)
+    ofile=open("frozen_phonons.log","w")
+    ofile.write(string)
 
+    # convert Mass to a.u.
+    M       =M*amu2au
 
-qe_input=Pwscf(scf_filename)
-qe_output=Pwout(qe_input)
+    # output reader
+    qe_output=Pwout(qe_input)
 
-# Setup pw.x executable
-pw="/home/attacc/SOFTWARE/qe-6.1/bin/pw.x"
-#pw="/home/elena/sources/qe-6.2/bin/pw.x"
+    scf_filename="scf.in"
 
-#Serial job
-#qe_input.set_run_options(pw=pw)
+    #Equilibrium calculation
+    folder="EQUIL"
+    qe_input.run(scf_filename,folder)
+    qe_output.read_output(scf_filename+".log", path=folder)
+    en_equil=qe_output.tot_energy
 
-#Parallel job
-qe_input.set_run_options(pw=pw, nprocs=2, npool=2)
+    # DFTP results
+    eig = np.array(qe_dyn.eig)
 
+    if modes == None:
+        modes = range(3, qe_dyn.nmodes) #skyp acustic modes at q=0
 
-# Pseudo-potential directory
-qe_input.control['pseudo_dir']="'/home/attacc/SOFTWARE/PSEUDO_PWSCF'"
+    for im in modes:
+        print(" Calculating mode %d .... " % (im+1))
+        if r_order == 1:
+            qe_right=qe_dyn.generate_displacement(0, im,  delta)
+            folder="RIGHT_"+str(im)
+            qe_right.run(scf_filename,folder)
+            qe_output.read_output(scf_filename+".log", folder)
+            en_right=qe_output.tot_energy
+            der2=2.0*(en_right-en_equil)/delta**2
 
-
-print("\n\n * * * Frozen phonon calculation * * *\n")
-print("Displacement    : %10.8f " % delta)
-print("Richardson order: %d " % r_order)
-
-# calculate the reduced mass
-masses=qe_input.get_masses()
-ref_mass=max(masses)
-M       =1.0/(np.sum(np.reciprocal(masses)))
-print("Reference  mass : %12.8f " % ref_mass)
-print("Reduced    mass : %12.8f [ref_mass units]  %12.8f  [amu]" % (M/ref_mass,M ))
-
-# convert Mass to a.u.
-M       =M*float(qe_input.system['nat'])*2.0
-M       =M*amu2au
-
-folder="EQUIL"
-qe_input.run(scf_filename,folder)
-qe_output.read_output(scf_filename+".log", path=folder)
-en_equil=qe_output.tot_energy
-qe_dyn=Matdyn(qe_input,dynmat_filename)
-eig = np.array(qe_dyn.eig)
-
-
-for im in range(3,qe_dyn.nmodes):   #skyp acustic modes at q=0
-    if r_order == 1:
-        qe_right=qe_dyn.generate_displacement(0, im,  delta)
-        folder="RIGHT_"+str(im)
-        qe_right.run(scf_filename,folder)
-        qe_output.read_output(scf_filename+".log", folder)
-        en_right=qe_output.tot_energy
-
-        der2=2.0*(en_right-en_equil)/delta**2
-
-    elif r_order == 2 or r_order == 3:
-        qe_right=qe_dyn.generate_displacement(0, im,  delta)
-        qe_left =qe_dyn.generate_displacement(0, im, -delta)
-        #
-        folder="LEFT_"+str(im)
-        qe_left.run(scf_filename,folder)
-        qe_output.read_output(scf_filename+".log", folder)
-        en_left=qe_output.tot_energy
-        #
-        folder="RIGHT_"+str(im)
-        qe_right.run(scf_filename,folder)
-        qe_output.read_output(scf_filename+".log", folder)
-        en_right=qe_output.tot_energy
-
-        der2=(en_right+en_left-2.0*en_equil)/delta**2
-
-        if r_order == 3:
-            der2_large=der2
-            qe_right=qe_dyn.generate_displacement(0, im,  delta/2.0)
-            qe_left =qe_dyn.generate_displacement(0, im, -delta/2.0)
+        elif r_order == 2 or r_order == 3:
+            qe_right=qe_dyn.generate_displacement(0, im,  delta)
+            qe_left =qe_dyn.generate_displacement(0, im, -delta)
             #
-            folder="LEFT_bis_"+str(im)
+            folder="LEFT_"+str(im)
             qe_left.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", folder)
             en_left=qe_output.tot_energy
             #
-            folder="RIGHT_bis_"+str(im)
+            folder="RIGHT_"+str(im)
             qe_right.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", folder)
             en_right=qe_output.tot_energy
 
-            der2_small=(en_right+en_left-2.0*en_equil)/(0.5*delta)**2
+            der2=(en_right+en_left-2.0*en_equil)/delta**2
 
-            der2=(4.0*der2_small-der2_large)/3.0
+            if r_order == 3:
+                der2_large=der2
+                qe_right=qe_dyn.generate_displacement(0, im,  delta/2.0)
+                qe_left =qe_dyn.generate_displacement(0, im, -delta/2.0)
+                #
+                folder="LEFT_bis_"+str(im)
+                qe_left.run(scf_filename,folder)
+                qe_output.read_output(scf_filename+".log", folder)
+                en_left=qe_output.tot_energy
+                #
+                folder="RIGHT_bis_"+str(im)
+                qe_right.run(scf_filename,folder)
+                qe_output.read_output(scf_filename+".log", folder)
+                en_right=qe_output.tot_energy
+
+                der2_small=(en_right+en_left-2.0*en_equil)/(0.5*delta)**2
+
+                der2=(4.0*der2_small-der2_large)/3.0
 
 
-    autosi=1.0/autime2s
-    omega=sqrt(der2/M)*autosi # We should understand this factor
+        autosi=1.0/autime2s
+        omega=sqrt(der2/M)*autosi/(2.0*math.pi)/1e12
 
-    print("Mode %d   fr.(THz)   %12.8f   [DFTP %12.8f]" % (im+1,omega/(2.0*math.pi)/1e12,eig[0,im]/thz2cm1))
-    print("Mode %d   fr.(cm^-1) %12.8f  [DFTP %12.8f] \n" % (im+1,omega/(2.0*math.pi)/1e12*thz2cm1,eig[0,im]))
+        diff=abs(omega/(eig[0,im]/thz2cm1)*100.0-100.0)
 
+        string ="Mode %d   fr.(THz)   %12.8f  [DFTP %12.8f]\n" % (im+1,omega,eig[0,im]/thz2cm1)
+        string+="Mode %d   fr.(cm^-1) %12.8f  [DFTP %12.8f]  DIFF  %7.3f%% \n" % (im+1,omega*thz2cm1,eig[0,im],diff)
 
+        print(string)
+        ofile.write(string)
+
+    ofile.close()
 
