3d8f946c9f9ca42760a1d1c11202ac580b7c6457
elenacannuccia <elena.cannuccia@gmail.com>
2017-12-01 23:20:00 +0100

    Some steps forward in zpr module

--- QEplayground/zpr.py
+++ QEplayground/zpr.py
@@ -16,8 +16,6 @@ import math
 def zpr(qe_input, qe_dyn, delta, r_order=2, modes=None):
     
     string="\n\n* * * Zero Point Motion calculations * * *\n\n"
-    string+="Reference  mass : %12.8f \n" % ref_mass
-    string+="Reduced    mass : %12.8f [ref_mass units]  %12.8f  [amu]\n" % (M/ref_mass,M )
 
     ofile=open("zpr.log","w")
     ofile.write(string)
@@ -31,7 +29,11 @@ def zpr(qe_input, qe_dyn, delta, r_order=2, modes=None):
     folder="EQUIL"
     qe_input.run(scf_filename,folder)
     qe_output.read_output(scf_filename+".log", path=folder)
-    dir_gap_eq, ind_gap_eq=qe_output.find_the_gap()
+    ival, icond, dir_gap_eq, ind_gap_eq=qe_output.find_the_gap()
+
+    # From the Equil *.save calculation the unperturbed eigenenergies are extracted 
+    prefix=qe_input.control['prefix'].strip('\'')
+    qe_xml=PwXML(prefix=prefix,path=folder)
 
     print("\n\n * * * ZPR contribution for the different phonons * * *\n")
     print("Displacement    : %10.8f " % delta)
@@ -40,8 +42,9 @@ def zpr(qe_input, qe_dyn, delta, r_order=2, modes=None):
     print("Equilibrium indirect gap: %12.8f " % (ind_gap_eq*ha2ev))
     print("Equilibrium direct gap  : %12.8f " % (dir_gap_eq*ha2ev))
 
-    # DFTP results
-    eig = np.array(qe_dyn.eig)
+    # DFT results
+    eigen=np.array(qe_xml.eigen)
+    
 
     if modes == None:
         modes = range(3, qe_dyn.nmodes) #skyp acustic modes at q=0
@@ -53,7 +56,7 @@ def zpr(qe_input, qe_dyn, delta, r_order=2, modes=None):
             folder="RIGHT_"+str(im)
             qe_right.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", path=folder)
-            dir_gap_right, ind_gap_right=qe_output.find_the_gap()
+            ival, icond, dir_gap_right, ind_gap_right=qe_output.find_the_gap()
             der2=2.0*(dir_gap_right-dir_gap_eq)/delta**2
 
         elif r_order == 2 or r_order == 3:
@@ -63,12 +66,14 @@ def zpr(qe_input, qe_dyn, delta, r_order=2, modes=None):
             folder="LEFT_"+str(im)
             qe_left.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", path=folder)
-            dir_gap_left, ind_gap_left=qe_output.find_the_gap()
+            ival, icond, dir_gap_left, ind_gap_left=qe_output.find_the_gap()
+            Gamma_val = np.array(eigen[0,ival])
+            print(Gamma_val)
             #
             folder="RIGHT_"+str(im)
             qe_right.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", path=folder)
-            dir_gap_right, ind_gap_right=qe_output.find_the_gap()
+            ival, icond, dir_gap_right, ind_gap_right=qe_output.find_the_gap()
 
             der2=(dir_gap_right+dir_gap_left-2.0*dir_gap_eq)/delta**2
 
@@ -80,12 +85,12 @@ def zpr(qe_input, qe_dyn, delta, r_order=2, modes=None):
                 folder="LEFT_bis_"+str(im)
                 qe_left.run(scf_filename,folder)
                 qe_output.read_output(scf_filename+".log", path=folder)
-                dir_gap_left, ind_gap_left=qe_output.find_the_gap()
+                ival, icond, dir_gap_left, ind_gap_left=qe_output.find_the_gap()
                 #
                 folder="RIGHT_bis_"+str(im)
                 qe_right.run(scf_filename,folder)
                 qe_output.read_output(scf_filename+".log", path=folder)
-                dir_gap_right, ind_gap_right=qe_output.find_the_gap()
+                ival, icond, dir_gap_right, ind_gap_right=qe_output.find_the_gap()
 
                 der2_small=(dir_gap_right+dir_gap_left-2.0*dir_gap_eq)/(0.5*delta)**2
 
