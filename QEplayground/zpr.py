d12f020cb6c3f08a8750e1586f33b4d3f023f4b3
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2017-12-13 22:45:03 +0100

    Multiple bands in the same calculation

--- QEplayground/zpr.py
+++ QEplayground/zpr.py
@@ -31,9 +31,13 @@ def zpr(qe_input, qe_dyn, delta, kp, bands, r_order=2, modes=None):
 
     string="\n\n* * * Zero Point Motion calculations * * *\n\n"
 
-    ofile=open("zpr_bands%s.log"%(bands+1),"w" )
+    ofile=open("zpr.log","w" )
     ofile.write(string)
-    
+    heading="#mode/band "
+    for count,band in enumerate(bands):
+        heading+="  "+str(band)+"       "
+    ofile.write(heading+"\n")
+
     # output reader
     qe_output=Pwout(qe_input)
 
@@ -44,7 +48,9 @@ def zpr(qe_input, qe_dyn, delta, kp, bands, r_order=2, modes=None):
     qe_input.run(scf_filename,folder)
 
     qe_output.read_output(scf_filename+".log", path=folder)
-    dft_energy_eq = qe_output.get_dft_energy(kp, bands)
+
+    bands_eq = np.asarray([qe_output.get_dft_energy(kp, band) for band in bands])
+
     dir_gap_eq, ind_gap_eq=qe_output.find_the_gap()
 
     print("\n\n * * * ZPR contribution for the different phonons * * *\n")
@@ -56,8 +62,9 @@ def zpr(qe_input, qe_dyn, delta, kp, bands, r_order=2, modes=None):
 
     if modes == None:
        modes = range(3, qe_dyn.nmodes) #skip acustic modes at q=0
-    
+
     der2_meV = 0.0
+
     for im in modes:
         print(" Calculating mode %d .... " % (im+1))
         if r_order == 1:
@@ -66,8 +73,8 @@ def zpr(qe_input, qe_dyn, delta, kp, bands, r_order=2, modes=None):
             folder="RIGHT_"+str(im)
             qe_right.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", path=folder)
-            dft_energy_right = qe_output.get_dft_energy(kp, bands)
-            der2=2.0*(dft_energy_right-dft_energy_eq)/delta**2
+            bands_right = np.asarray([qe_output.get_dft_energy(kp, band) for band in bands])
+            der2=2.0*(bands_right-bands_eq)/delta**2
 
         elif r_order == 2 or r_order == 3:
             qe_right=qe_dyn.generate_displacement(0, im,  delta)
@@ -76,14 +83,14 @@ def zpr(qe_input, qe_dyn, delta, kp, bands, r_order=2, modes=None):
             folder="LEFT_"+str(im)
             qe_left.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", path=folder)
-            dft_energy_left = qe_output.get_dft_energy(kp, bands)
+            bands_left = np.asarray([qe_output.get_dft_energy(kp, band) for band in bands])
             #
             folder="RIGHT_"+str(im)
             qe_right.run(scf_filename,folder)
             qe_output.read_output(scf_filename+".log", path=folder)
-            dft_energy_right = qe_output.get_dft_energy(kp, bands)
+            bands_right = np.asarray([qe_output.get_dft_energy(kp, band) for band in bands])
 
-            der2=(dft_energy_right+dft_energy_left-2.0*dft_energy_eq)/delta**2
+            der2=(bands_right+bands_left-2.0*bands_eq)/delta**2
 
             if r_order == 3:
                 der2_large=der2
@@ -93,24 +100,27 @@ def zpr(qe_input, qe_dyn, delta, kp, bands, r_order=2, modes=None):
                 folder="LEFT_bis_"+str(im)
                 qe_left.run(scf_filename,folder)
                 qe_output.read_output(scf_filename+".log", path=folder)
-                dft_energy_left = qe_output.get_dft_energy(kp, bands)
+                bands_left = np.asarray([qe_output.get_dft_energy(kp, band) for band in bands])
                 #
                 folder="RIGHT_bis_"+str(im)
                 qe_right.run(scf_filename,folder)
                 qe_output.read_output(scf_filename+".log", path=folder)
-                dft_energy_right = qe_output.get_dft_energy(kp, bands)
+                bands_right = np.asarray([qe_output.get_dft_energy(kp, band) for band in bands])
                 #
-                der2_small=(dft_energy_right+dft_energy_left-2.0*dft_energy_eq)/(0.5*delta)**2
-
+                der2_small=(bands_right+bands_left-2.0*bands_eq)/(0.5*delta)**2
                 der2 = (4.0*der2_small-der2_large)/3.0
-                der2 = der2/2/M/(2.0*math.pi)/DFTP_freq[0,im]/2  # Last division by 2 is the 1/2 of the zpr
 
-        string = "Mode %d   der2 gap   %12.8f \n"  % (im+1, der2)
-        print(string)
+
+        values =str(im+1)+"         "
+        for count in range(len(bands)):
+            values +="  %12.8f      "% (der2[count])
+        ofile.write(values+"\n")
+
+        der2 = der2/2/M/(2.0*math.pi)/DFTP_freq[0,im]/2  # Last division by 2 is the 1/2 of the zpr
         der2_meV += der2*ha2ev/1000
-        ofile.write(string)
 
-    string = "ZPR on k = (0,0,0) and band = %s: %12.8f meV" % (bands+1, der2_meV)
-    print(string)
+    for count, band in enumerate(bands):
+        string = "ZPR on k = (0,0,0) and band = %s: %12.8f meV" % (band+1, der2_meV[count])
+        print(string)
 
     ofile.close()
