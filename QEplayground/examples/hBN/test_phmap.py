f807df819d8a433e6864605c8964f81f468b611d
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2018-04-09 17:27:43 +0200

    Start testing phonon mapping

---
+++ QEplayground/examples/hBN/test_phmap.py
@@ -0,0 +1,19 @@
+from QEplayground.pwscf  import *
+from QEplayground.matdyn import *
+from QEplayground.ph2supercell import *
+
+
+scf_filename    ="hBN.scf.in"
+dynmat_filename ="joint_dynmat.out"
+
+q_grid=[2,2,2]
+
+qe_input =Pwscf(scf_filename)
+qe_dyn=Matdyn(qe_input,dynmat_filename)
+
+
+ph_map=map_phonons(qe_input, qe_dyn, q_grid)
+
+ph_map.build_mapping()
+
+
